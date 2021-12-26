e40f7ebe27bd0e8a02c04bd9cae9ad5f7883621d
Andrey Shirobokov <loadtest@mail.ru>
2021-12-26 13:29:38 +0300

    Java blog app. Initial commit.

--- src/main/java/ru/ashirobokov/blog/cache/CacheImpl.java
+++ src/main/java/ru/ashirobokov/blog/cache/CacheImpl.java
@@ -3,8 +3,6 @@ package ru.ashirobokov.blog.cache;
 import lombok.extern.slf4j.Slf4j;
 import ru.ashirobokov.blog.model.Article;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
 
@@ -12,19 +10,19 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 /***
  *   Cache eviction policies :
- *   1. Based on cache size (defaultCacheSize);
- *   2. Based on cached time (defaultCachedTime) in seconds;
- *
+ *      1. Based on cache size (defaultCacheSize);
+ *      2. Based on cached time (defaultCachedTime) in seconds;
+ *      3. Based on access frequency;
  *
  */
 @Slf4j
 public class CacheImpl implements Cache<Object, Object> {
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
     private ConcurrentMap<Object, Object> cache = new ConcurrentHashMap<>();
-//    private List<Point> cacheStructure = new LinkedList<>();
+    //    private List<Point> cacheStructure = new LinkedList<>();
     private Queue<Point> cacheStructure = new ConcurrentLinkedQueue<>();
     private int defaultCacheSize = 5;
-    private int defaultCachedTime = 15; // 3200
+    private int defaultCachedTimeMillis = 60 * 10 * 1000; // 3200
 
     /**
      * Using Singleton static Holder pattern for cache
@@ -41,14 +39,22 @@ public class CacheImpl implements Cache<Object, Object> {
     @Override
     public void put(Object key, Object value) {
         log.info("[Cache] put Object {} with key = {}", value, key);
-        cacheStructure.add(new Point(key, System.currentTimeMillis()));
+        cacheStructure.add(new Point(key, System.currentTimeMillis(), 0L));
         cache.put(key, value);
+//        print();
     }
-//  Cache eviction policies
 
     @Override
     public Object get(Object key) {
         log.info("[Cache] get by key = {}", key);
+        cacheStructure.stream()
+                .filter(p -> p.getKey().equals(key))
+                .forEach(point -> {
+                    long counter = point.getAccessCounter();
+                    log.info("[Cache] Old access counter value = {}", counter);
+                    point.setAccessCounter(++counter);
+                });
+
         return cache.get(key);
     }
 
@@ -78,18 +84,21 @@ public class CacheImpl implements Cache<Object, Object> {
         return cache.size();
     }
 
+    private void print() {
+        cacheStructure.stream().forEach(point -> log.info("[Cache] print Point = {}", point));
+    }
+
     private final void cacheSizeCheck() {
         log.info("[Cache-Size-Policy] Cache Size Check started at {}", System.currentTimeMillis());
         final Runnable sizeChecker = () -> {
             int currentCacheSize = cache.size();
-            if (currentCacheSize > defaultCacheSize) {
+            while (currentCacheSize > defaultCacheSize) {
                 log.info("[Cache-Size-Policy] Current Cache Size exeeded the allowed cache size. Removing data from cache");
                 Point point = cacheStructure.stream().findFirst().get();
                 cache.remove(point.getKey());
                 cacheStructure.remove(point);
-                log.info("[Cache-Size-Policy] Cache Point {} was removed", point);
-            } else {
-                log.debug("[Cache-Size-Policy] Current Cache Size is not exeeded yet");
+                currentCacheSize = cache.size();
+                log.info("[Cache-Size-Policy] Cache Point {} was removed, current Cache size = {}", point, currentCacheSize);
             }
         };
 
@@ -102,17 +111,22 @@ public class CacheImpl implements Cache<Object, Object> {
         final Runnable timeChecker = () -> {
             try {
                 long currentTime = System.currentTimeMillis();
-                long evictionTime = currentTime - defaultCachedTime * 1000;
+                long evictionTime = currentTime - defaultCachedTimeMillis;
                 log.debug("[DEBUG] Cache Structure = {}, currentTime = {}, evictionTime = {}", cacheStructure.toString(), currentTime, evictionTime);
                 cacheStructure.stream()
                         .takeWhile(point -> point.getSavedTime() < evictionTime)
                         .forEach(point -> {
-                                    log.info("[Cache-Time-Policy] Cache Point {} was removed after {} sec",
-                                            point, TimeUnit.MILLISECONDS.toSeconds(point.getSavedTime()));
-                                    Article article = (Article) cache.remove(point.getKey());
-                                    log.debug("[DEBUG] Removed from cache {}", article);
-                                    boolean removedOk = cacheStructure.remove(point);
-                                    log.debug("[DEBUG] Removed from cacheStructure {}", removedOk);
+                                    long counter = point.getAccessCounter();
+                                    if (counter > 0) {
+                                        point.setAccessCounter(--counter);
+                                    } else {
+                                        log.info("[Cache-Time-Policy] Cache Point {} is expected to be removed after {} sec",
+                                                point, TimeUnit.MILLISECONDS.toSeconds(currentTime - point.getSavedTime()));
+                                        Article article = (Article) cache.remove(point.getKey());
+                                        log.debug("[DEBUG] Removed from cache {}", article);
+                                        boolean removedOk = cacheStructure.remove(point);
+                                        log.debug("[DEBUG] Removed from cacheStructure {}", removedOk);
+                                    }
                                 }
                         );
             } catch (Exception e) {
--- src/main/java/ru/ashirobokov/blog/cache/Point.java
+++ src/main/java/ru/ashirobokov/blog/cache/Point.java
@@ -1,19 +1,18 @@
 package ru.ashirobokov.blog.cache;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @ToString
 public class Point {
 
     private Object key;
     private long savedTime;
+    private long accessCounter;
 
 }
